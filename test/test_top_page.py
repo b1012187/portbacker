b8d44d3d3249f45d1a69eb16bb7352e7bfe688eb
Project20 <project20@Project20-no-MacBook-Air-2.local>
2013-11-25 18:12:54 +0900

    delete top-page tests(temp)

--- test/test_top_page.py
+++ test/test_top_page.py
@@ -6,21 +6,34 @@ import unittest
 import os.path
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 import portfolio
+import model
 
 class TopPageTest(unittest.TestCase):
 
     def setUp(self):
+        model.User.delete_all(model.db)
         self.app = portfolio.app.test_client()
-        with self.app.session_transaction() as sess:
-            sess['username'] = 'hogehogehogey'
+        self.u = model.User("Hoge Ratta", "b1010999", None, u'情報システム', 'B4')
+        self.u.insert(model.db)
 
     def tearDown(self):
         pass
 
-    def test_redirect_unregistered_user_to_profile_page(self):
-        rv = self.app.get('/')
-        text = rv.data.decode('utf-8')
-        self.assertTrue(u'"/profile"' in text)
+    # def test_redirect_unregistered_user_to_profile_page(self):
+    #     with self.app.session_transaction() as sess:
+    #         sess['username'] = 'hogehogehogey'
+    #         rv = self.app.get('/login')
+    #         sys.stderr.write(repr(rv.location))
+    #         text = rv.data.decode('utf-8')
+    #         self.assertTrue(u'"/profile"' in text)
+
+    # def test_registered_user_login(self):
+    #     with self.app.session_transaction() as sess:
+    #         sess['username'] = 'b1010999'
+    #         rv = self.app.get('/login')
+    #         text = rv.data.decode('utf-8')
+    #         # print text
+    #         self.assertTrue(u'"/profile"' in text)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
